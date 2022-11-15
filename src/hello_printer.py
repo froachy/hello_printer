05d38f091cfe66b6e8d81118379fc7631c20e0e3
Brandon Evans <froachy@gmail.com>
2022-11-14 20:07:49 -0600

    Initial commit

---
+++ src/hello_printer.py
@@ -0,0 +1,11 @@
+class HelloPrinter:
+    def __init__(self):
+        pass
+
+    def print_hello(self):
+        print("Hello, world!")
+
+
+if __name__ == "__main__":
+    hp = HelloPrinter()
+    hp.print_hello()
